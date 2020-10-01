3bc4f8013148b111adf7e26926e94213ee583070
Carlos <carlosco96@hotmail.com>
2020-10-01 07:29:30 -0500

    Firebase connected and some conditionals tested. Weirdly, dunno why, when i try to insert the message function of Twilio inside a firebase search query conditional, it does not go through. Solution that i found is to usea a boolean. But, no really now if thats the way it should be done.

--- server/index.js
+++ server/index.js
@@ -1,68 +1,123 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const express = require("express");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+var admin = require("firebase-admin");
 
-const client = require('twilio')(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_AUTH_TOKEN
-  );
-  
+var serviceAccount = require("../flordb-38125-firebase-adminsdk-4c2hv-611ecdc39b.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://flordb-38125.firebaseio.com",
+});
+
+const db = admin.database();
+
+const client = require("twilio")(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(pino);
 
-
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
+app.get("/api/greeting", (req, res) => {
+  const name = req.query.name || "World";
+  res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({ greeting: `Hellos ${name}!` }));
 });
 
-app.post('/inbound', (req, res) => {
-      // Start our TwiML response.
-      const twiml = new MessagingResponse();
-      const message = twiml.message();
-      const body = req.body.Body.toLowerCase();
-      const from = req.body.From;
-      console.log(body); 
-      if (body.includes("janneth")) {
-        // If the message includes "contact" send a VCard with contact details
-        message.body(
-            "Vitamina C"
-          );
-      }else {
-        // Otherwise send a formatted message.
-        message.body(
-          "No entendí muy bien lo que dijiste"
-        );
-        }
-        
-      res.writeHead(200, {'Content-Type': 'text/xml'});
-      res.end(twiml.toString());
-  });
+const getName = function (input) {
+  if (input.includes("mi nombre es")) {
+    const searchTerm = "es";
+    const index = input.lastIndexOf(searchTerm);
+    const name = input.slice(index);
+    console.log(index);
+    return name;
+  } else {
+    return "";
+  }
+};
 
-app.post('/api/messages', (req, res) => {
-  res.header('Content-Type', 'application/json');
+app.post("/inbound", (req, res) => {
+  // Start our TwiML response.
+  const twiml = new MessagingResponse();
+  const message = twiml.message();
+  const body = req.body.Body.toLowerCase();
+  const from = req.body.From;
+  var isNew = ""; 
+  console.log(body);
+
+  if(isNew){
+    message.body("*Hola* Me llamo *Flor*, ¿Cuál es tu nombre?");
+  }else if(!isNew){
+    message.body("Bienvenido de nuevo");
+  }
+  
+  if (body.includes("hola")) {
+    db.ref()
+      .child("contacts")
+      .orderByChild("number")
+      .equalTo(req.body.From)
+      .once("value", (snapshot) => {
+        if (!snapshot.exists()) {
+          const message = twiml.message();
+          const newContact = {
+            number: from,
+            name: "",
+            location: "",
+            age: "",
+          };
+          db.ref("contacts").push(newContact);
+         isNew = true; 
+         console.log("i dontknow this guy" +isNew);
+        } else {
+          isNew = false;
+          console.log("i know this guy" +isNew);
+          console.log("exists!", snapshot.exists());
+        }
+      });
+  } else if (body.includes("nombre") || body.includes("llamo")) {
+    db.ref
+      .child("users")
+      .orderByChild("ID")
+      .equalTo(req.body.From)
+      .once("value", (snapshot) => {
+        if (snapshot.exists()) {
+          usersRef.child(req.body.From).set({
+            name: req.body.Body,
+          });
+          console.log("exists!", userData);
+        }
+      });
+  } else {
+    // Otherwise send a formatted message.
+    message.body("No entendí muy bien lo que dijiste");
+  }
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(twiml.toString());
+});
+
+app.post("/api/messages", (req, res) => {
+  res.header("Content-Type", "application/json");
   client.messages
     .create({
-      from: 'whatsapp:'+ process.env.TWILIO_PHONE_NUMBER,
-      to: 'whatsapp:'+ req.body.to,
-      body: req.body.body
+      from: "whatsapp:" + process.env.TWILIO_PHONE_NUMBER,
+      to: "whatsapp:" + req.body.to,
+      body: req.body.body,
     })
     .then(() => {
       res.send(JSON.stringify({ success: true }));
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.send(JSON.stringify({ success: false }));
     });
-}); 
-
+});
 
 app.listen(5000, () => {
-
-    console.log("Express is running in localhost:5000");
-
+  console.log("Express is running in localhost:5000");
 });
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SMSForm from './SMSForm';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SMSForm from "./SMSForm";
 
 class App extends Component {
   render() {
