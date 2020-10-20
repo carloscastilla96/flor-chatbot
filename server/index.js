5266d71f767f279d963f6a3939f99098183bfd9a
Carlos <carlosco96@hotmail.com>
2020-10-20 14:19:13 -0500

    last code before implementing NLP.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
- /*
+/*
 Resources for connection with express and React 
 --> https://www.twilio.com/blog/react-app-with-node-js-server-proxy
 --> https://github.com/philnash/send-sms-react-twilio/blob/master/server/index.js
@@ -22,11 +22,17 @@ Twilio media receive and send
 -->https://support.twilio.com/hc/en-us/articles/360017961894-Sending-and-Receiving-Media-with-WhatsApp-Messaging-on-Twilio-Beta-
 How to make it safe 
 --> https://medium.com/@victorpaulo/acme-whatsapp-banking-twilio-setup-ba562e69c50e
+HOw to send Emojis 
+https://www.twilio.com/blog/2018/08/emoji-translations-twilio-api-whatsapp-node-js.html
+Get a Number https://www.twilio.com/docs/whatsapp/api
+https://www.twilio.com/whatsapp/request-access
+
 */
 const express = require("express");
 const bodyParser = require("body-parser");
 const pino = require("express-pino-logger")();
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const tunnel = 'https://dff4f3cf982a.ngrok.io';
 var admin = require("firebase-admin");
 
 var serviceAccount = require("../flordb-38125-firebase-adminsdk-4c2hv-611ecdc39b.json");
@@ -58,23 +64,65 @@ app.get("/api/greeting", (req, res) => {
   res.send(JSON.stringify({ greeting: `Hellos ${name}!` }));
 });
 
-const getName = function (input) {
-  const searchTerm = "llamo";
-  var index = input.search(searchTerm);
-  console.log("index es" + index);
-  var name = input.slice(index);
-  console.log("name es" + name);
+app.get('/:userId', function (req, res) {
+
+  console.log(req.params);
+  res.setHeader("Content-Type", "application/json");
+  db.ref("users")
+    .child(req.params.userId)
+    .once("value")
+    .then(function (snapshot) {
+      var snap = snapshot.val();
+      if (!snapshot.exists()) {
+        res.send(JSON.stringify("nothing here"));
+      } else {
+        res.send(JSON.stringify({ greeting: `Hello ${snap.name}!`, stage: `Hello ${snap.stage}!` }));
+      }
+    });
+});
+
+
+
+const getName = function (input, type) {
+  var name = input.toLowerCase;
+  var searchTerm;
+  switch (type) {
+    case 1:
+      var searchTerm = "llamo";
+      var index = searchTerm.lastIndexOf();
+      console.log(index);
+      var name = input.slice(index);
+      break;
+    case 2:
+      var searchTerm = "nombre es";
+      var index = searchTerm.lastIndexOf();
+      console.log(index);
+      var name = input.slice(index);
+      break;
+    default:
+      var searchTerm = "";
+      var index = searchTerm.lastIndexOf();
+      console.log(index);
+      var name = input.slice(index);
+      break;
+  }
+
   return name;
 };
+
+const getNumber = (input) => {
+  var index = input.lastIndexOf("+");
+  var number = input.slice(index);
+  return number;
+};
+
 // Twilio and Firebase working together https://stackoverflow.com/questions/50182368/why-is-twilio-and-firebase-not-working-cohesively-using-node-js
 app.post("/inbound", async (req, res) => {
   // Start our TwiML response.
   const twiml = new MessagingResponse();
   const messages = twiml.message();
   const body = req.body.Body.toLowerCase();
-  const from = req.body.From;
-  var isNew;
-  console.log(body);
+  const from = getNumber(req.body.From);
 
   if (
     body.includes("hola") ||
@@ -82,51 +130,61 @@ app.post("/inbound", async (req, res) => {
     body.includes("buenas")
   ) {
     var NewUserMessage = "";
-    db.ref()
-      .child("contacts")
-      .orderByChild("number")
-      .equalTo(req.body.From)
+    db.ref("users")
+      .child(from)
       .once("value")
       .then(function (snapshot) {
         if (!snapshot.exists()) {
-          db.ref("contacts/users").push({
-            number: from,
-            title: 1,
+
+          db.ref("users").child(from).set({
+            stage: 1,
           });
           NewUserMessage =
-            "¡Buenas! Soy Luz y trabajo con la iniciativa Sembrando vida Me fascina la naturaleza y la conexión que tenemos con ella. Me gusta conocer y ayudar a personas que también les guste.  Contame, ¿Cómo te llamas?  ";
+            "¡Buenas! Soy Luz y trabajo con la iniciativa Sembrando vida Me fascina la naturaleza y la conexión que tenemos con ella. Me gusta conocer y ayudar a personas que también les guste.  Contame, ¿Cómo te llamas?";
         } else {
-          NewUserMessage =
-            "¡Buenas! Se me olvidó tu nombre. Me lo puedes recordar?";
+          var snap = snapshot.val();
+          var name = snap.name;
+          if (!snap.name) {
+            NewUserMessage =
+              "¡Buenas! Se me olvidó tu nombre. Me lo puedes recordar?";
+          } else {
+            NewUserMessage =
+              `${tunnel} / ${from}`;
+          }
+
         }
         return NewUserMessage;
       })
       .then(function (NewUserMessage) {
-        console.log(NewUserMessage);
         messages.body(NewUserMessage);
         res.writeHead(200, { "Content-Type": "text/xml" });
         res.end(twiml.toString());
       });
   } else if (body.includes("nombre") || body.includes("llamo")) {
     var nameMessage = "";
-    db.ref()
-      .child("contacts")
-      .orderByChild("number")
-      .equalTo(req.body.From)
+    db.ref("users")
+      .child(from)
       .once("value")
       .then(function (snapshot) {
-       var titleSnap= snapshot.child("contacts/users").val(); 
-        if ( titleSnap.title > 0) {
-          db.ref("contacts/users").set({
-            number: from,
-            title: 1,
-            name: body,
+        var snap = snapshot.val();
+
+        if (snap.stage > 0) {
+          var userName;
+          if (body.includes("llamo")) {
+            var userName = getName(body, 1);
+          } else if (body.includes("nombre")) {
+            var userName = getName(body, 2);
+          } else {
+            var userName = getName(body, 0);
+          }
+
+          db.ref("users").child(from).set({
+            stage: 2,
+            name: userName,
           });
-          var nameF = getName(body);
+
           nameMessage =
-            "Mucho Gusto," +
-            nameF +
-            "!Espero poder ayudarte y conocer más sobre vos";
+            `Mucho Gusto, ${userName}!Espero poder ayudarte y conocer más sobre vos`;
         } else {
           nameMessage =
             '¡Lo siento! No pude registrar tu nombre. Prueba diciendo " Me llamo " seguido de tu nombre';
@@ -148,22 +206,6 @@ app.post("/inbound", async (req, res) => {
   }
 });
 
-app.post("/api/messages", (req, res) => {
-  res.header("Content-Type", "application/json");
-  client.messages
-    .create({
-      from: "whatsapp:" + process.env.TWILIO_PHONE_NUMBER,
-      to: "whatsapp:" + req.body.to,
-      body: req.body.body,
-    })
-    .then(() => {
-      res.send(JSON.stringify({ success: true }));
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send(JSON.stringify({ success: false }));
-    });
-});
 
 app.listen(5000, () => {
   console.log("Express is running in localhost:5000");
